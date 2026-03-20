50b098f4d24b78eb130c2bdb8df99d5b8a462948
beganime <begenchyagmurow2008@gmail.com>
2026-03-20 23:21:30 +0500

    fix url

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, bot, devices, export, internal_bots, users, websockets, chats, storage, search # Добавили chats
+from app.api.v1.endpoints import auth, bot, devices, export, internal_bots, users, websockets, chats, pages, storage, search # Добавили chats
 
 api_router = APIRouter()
 
@@ -13,3 +13,4 @@ api_router.include_router(bot.router, prefix="/bot", tags=["bot"])
 api_router.include_router(devices.router, prefix="/devices", tags=["devices"])
 api_router.include_router(export.router, prefix="/export", tags=["export"])
 api_router.include_router(internal_bots.router, prefix="/internal/bots", tags=["internal"])
+api_router.include_router(pages.router, prefix="", tags=["pages"])
